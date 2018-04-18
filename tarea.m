42de3f10268258cf60bb6c1deb8755be2f087b71
Rentaoh <falcon_p.a@hotmail.com>
2018-04-18 16:36:51 -0300

    avance 18/4/2016

--- tarea.m
+++ tarea.m
@@ -1,26 +1,16 @@
-function main
-datay=dlmread('c:\Users\araya\Desktop\inteligencia de negocis\Data\dtst_y')
-datax=dlmread('c:\Users\araya\Desktop\inteligencia de negocis\Data\dtst_x')
+
+datax=dlmread('C:\Users\araya\Desktop\inteligencia de negocis\tarea 1\data\grupo3.txt')
+datay=zeros(200,1)
+for i = 1:100
+  datay(i)=1
+endfor  
 maxiter=5*exp(3)
 mu=1*exp(-3)
 Hlayer=[40 20]
-W=mlp_inic_w(size(datax),size(datay),Hlayer)
+W=mlp_inic_w(size(datax,2),size(datay,2),Hlayer)
 mlp=mlp_train(datax,datay,W,maxiter)
-zv=mlp_test()
-endfunction
+#zv=mlp_test()
 
-function [W] = mlp_inic_w(nlnp,mOut,Hlayer)
- H=[nlnp Hlayer mOut]
- L=numel(H)
- x=nlnp+mOut
- print(x)
- a=6/sqrt(x)
- for i= 1:L-1
-   W(i)=rand(H(i),H(i+1))*2*a-a
- endfor
- return
-endfunction
 
-function [mse]= mlp_train(datax,datay,W,maxiter)
-  L=length(W)
-endfunction
+
+
